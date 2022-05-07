365b3e34f71706ec299b90d31cbe4e979b54b026
vanyauhalin <vanyauhalin@gmail.com>
2022-05-07 12:40:56 +0300

    feat: simplify create fn, add ctx

--- src/script.ts
+++ src/script.ts
@@ -1,58 +1,45 @@
 import kleur from 'kleur';
-import type { Script, ScriptCallback } from 'types';
+import type { Script, ScriptCallback, ScriptContext } from 'types';
 import { log } from './logger';
 import { isAsync, stopwatch } from './utils';
 
-function create(): Script {
-  const rejected = new Set<string>();
-  const running = new Set<string>();
-  const scripts = new Map<string, ScriptCallback<unknown>>();
-  function empty(): void {
-    if (!running.size) log.empty();
-  }
-  function inner<T extends unknown>(
-    event: string,
-    callback: ScriptCallback<T>,
-  ): void {
-    scripts.set(event, callback);
-  }
-  inner.run = (
-    event = process.env['npm_lifecycle_event'],
-  ) => new Promise((resolve) => {
-    const { lap } = stopwatch();
+function init(ctx: ScriptContext): Script {
+  return ((event, callback) => {
+    ctx.scripts.set(event, callback);
+  }) as Script;
+}
 
-    if (!event) {
-      log.error.trace('No script to run');
-      return;
-    }
-    if (rejected.size) return;
+function runner(ctx: ScriptContext, event: string): Promise<unknown> {
+  return new Promise((resolve) => {
+    const { lap } = stopwatch();
+    if (ctx.rejected.size) return;
     const colored = kleur.blue(event);
 
-    running.add(event);
+    ctx.running.add(event);
     log(`Running ${colored} ...`);
 
     const reject = (message: string): void => {
-      running.delete(event);
-      rejected.add(event);
+      ctx.running.delete(event);
+      ctx.rejected.add(event);
       log.error.trace(message);
-      empty();
+      if (!ctx.running.size) log.empty();
     };
-    if (!scripts.has(event)) {
+    if (!ctx.scripts.has(event)) {
       reject(`${colored} not found`);
       return;
     }
-    const callback = scripts.get(event);
+    const callback = ctx.scripts.get(event);
     if (!callback) {
       reject(`Callback for ${colored} not found`);
       return;
     }
 
     const done = (returns: unknown): void => {
-      log[rejected.size
+      ctx.running.delete(event);
+      log[ctx.rejected.size
         ? 'error'
         : 'done'](`Finished ${colored} after ${lap()}`);
-      running.delete(event);
-      empty();
+      if (!ctx.running.size) log.empty();
       resolve(returns);
     };
     const returns = callback();
@@ -62,7 +49,23 @@ function create(): Script {
     }
     done(returns);
   });
-  return inner;
+}
+
+function create(): Script {
+  const ctx = {
+    rejected: new Set<string>(),
+    running: new Set<string>(),
+    scripts: new Map<string, ScriptCallback<unknown>>(),
+  };
+  const script = init(ctx);
+  script.run = async (event = process.env['npm_lifecycle_event']) => {
+    if (!event) {
+      log.error.trace('No script to run');
+      return;
+    }
+    await runner(ctx, event);
+  };
+  return script;
 }
 
 export {
