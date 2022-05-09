663e6b963251963b2a5892d0d07e648d6652fa40
vanyauhalin <vanyauhalin@gmail.com>
2022-05-09 19:02:52 +0300

    feat: add scan fn

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import type { Context } from 'types';
 import { log } from './logger';
+import { create as createScan } from './scan';
 import { create as createScript } from './script';
 
 const ctx: Context = {
   rejected: [],
   scripts: {},
 };
+const scan = createScan(ctx);
 const script = createScript(ctx);
 
 export {
   log,
+  scan,
   script,
 };
