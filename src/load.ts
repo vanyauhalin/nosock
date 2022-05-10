4e4ae56d5ee3cbf5a6c3621f40f97c1e9d015510
vanyauhalin <vanyauhalin@gmail.com>
2022-05-10 12:01:13 +0300

    fix: remove options for load

--- src/load.ts
+++ src/load.ts
@@ -2,10 +2,8 @@ import { readdir } from 'fs';
 import { resolve } from 'path';
 import { promisify } from 'util';
 
-async function load(opts: {
-  cwd?: string;
-}): Promise<void> {
-  const cwd = resolve(opts.cwd || '..');
+async function load(): Promise<void> {
+  const cwd = resolve('.');
   const rc = (await promisify(readdir)(cwd))
     .find((el) => /\.?((scripts|scer)(rc)?)\.([cm]js|[jt]s)/.test(el));
   if (!rc) throw new Error('Scripts file not found');
