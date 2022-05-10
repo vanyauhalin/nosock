f4601864820a328f018b885aae75b72d4746c3b1
vanyauhalin <vanyauhalin@gmail.com>
2022-05-10 12:01:40 +0300

    feat: run exec in load

--- src/load.ts
+++ src/load.ts
@@ -1,6 +1,7 @@
 import { readdir } from 'fs';
 import { resolve } from 'path';
 import { promisify } from 'util';
+import { exec } from './script';
 
 async function load(): Promise<void> {
   const cwd = resolve('.');
@@ -8,6 +9,7 @@ async function load(): Promise<void> {
     .find((el) => /\.?((scripts|scer)(rc)?)\.([cm]js|[jt]s)/.test(el));
   if (!rc) throw new Error('Scripts file not found');
   await import(`${cwd}/${rc}`);
+  await exec();
 }
 
 export {
