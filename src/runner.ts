19ece0f249a81bcbf440f26837058ec875cde123
vanyauhalin <vanyauhalin@gmail.com>
2022-05-27 15:23:51 +0300

    refactor: move runner to own file

---
+++ src/runner.ts
@@ -0,0 +1,26 @@
+import kleur from 'kleur';
+import { log } from './log';
+import type { Context, ContextScript } from './script';
+import { stopwatch } from './utils';
+
+async function run(context: Context, script: ContextScript): Promise<unknown> {
+  const lap = stopwatch();
+  if (context.rejected) return undefined;
+  const { callback, command } = script;
+  const colored = kleur.blue(command);
+  log(`Running ${colored} ...`);
+  let result;
+  try {
+    result = await callback();
+  } catch (error) {
+    const errored = error as Error;
+    context.rejected = +1;
+    log.error(errored.message).trace(errored);
+  }
+  log[context.rejected
+    ? 'error'
+    : 'done'](`Finished ${colored} after ${lap()}`);
+  return result;
+}
+
+export { run };
--- src/script.ts
+++ src/script.ts
@@ -1,36 +1,16 @@
 import { exit } from 'node:process';
-import kleur from 'kleur';
 import { log } from './log';
+import { run } from './runner';
 import { scan } from './scanner';
-import { stopwatch } from './utils';
 
 interface Context {
   rejected: number;
   scripts: Record<string, ContextScript>;
 }
 type ContextScript = {
-  cmd: string;
-  cb(): Promise<unknown>;
+  command: string;
+  callback(this: void): Promise<unknown>;
 };
-
-async function run(
-  ctx: Context,
-  script: ContextScript,
-): Promise<unknown> {
-  const lap = stopwatch();
-  if (ctx.rejected) return undefined;
-  const colored = kleur.blue(script.cmd);
-  log(`Running ${colored} ...`);
-  let result;
-  try {
-    result = await script.cb();
-  } catch (err) {
-    ctx.rejected = +1;
-    log.error((err as Error).message).trace(err as Error);
-  }
-  log[ctx.rejected ? 'error' : 'done'](`Finished ${colored} after ${lap()}`);
-  return result;
-}
 
 const { script, exec } = (() => {
   const ctx: Context = {
@@ -38,15 +18,15 @@ const { script, exec } = (() => {
     scripts: {},
   };
   return {
-    script: ((cmd, cb) => {
+    script: ((command, callback) => {
       const cur: ContextScript = {
-        cmd,
-        cb: () => Promise.resolve(cb()),
+        command,
+        callback: () => Promise.resolve(callback()),
       };
-      ctx.scripts[cmd] = cur;
+      ctx.scripts[command] = cur;
       return run.bind(null, ctx, cur);
     }) as {
-      <C extends () => unknown>(cmd: string, cb: C): () => (
+      <C extends () => unknown>(command: string, callback: C): () => (
         Promise<C extends () => Promise<unknown>
           ? Awaited<ReturnType<C>>
           : ReturnType<C>>
