d6b314d97ce33bf587f4815fb8b062a6a297216d
vanyauhalin <vanyauhalin@gmail.com>
2022-05-27 15:45:15 +0300

    refactor: update index due to refactor other files

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
+import { define as defineContext } from './context';
+import { define as defineExecutor } from './executor';
+import { define as defineScript } from './scripter';
+
+const { exec, script } = (() => {
+  const context = defineContext();
+  return {
+    exec: defineExecutor(context),
+    script: defineScript(context),
+  };
+})();
+
+export * from './utils';
+export { exec, script };
 export { log } from './log';
-export { script } from './script';
