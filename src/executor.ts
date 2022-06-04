59945e4203b2b95e4f40d5109fa2de8d069a1256
vanyauhalin <vanyauhalin@gmail.com>
2022-06-04 15:15:29 +0300

    feat: handle error when execute command

--- src/executor.ts
+++ src/executor.ts
@@ -1,18 +1,27 @@
 import { env, stdout } from 'node:process';
 import type { Context } from './context';
-import { log } from './logger';
+import { ACCENT, log } from './logger';
 import { run } from './runner';
+import { stopwatch } from './utils';
 
 function define(context: Context) {
   return async (file?: string): Promise<void> => {
+    const lap = stopwatch();
     stdout.write('\n');
     if (file) log('Runnings scripts...').note(file);
-    const command = env['npm_lifecycle_event'];
-    if (!command) throw new Error('Missing a run command');
-    const script = context.scripts[command];
-    if (!script) throw new Error(`The ${command} is not described`);
-    await run(context, script);
-    stdout.write('\n');
+    try {
+      const command = env['npm_lifecycle_event'];
+      if (!command) throw new Error('Missing a run command');
+      const script = context.scripts[command];
+      if (!script) throw new Error(`The ${ACCENT(command)} is not described`);
+      await run(context, script);
+    } catch (error) {
+      const { message } = error as Error;
+      log.error(message);
+      throw new Error(message);
+    } finally {
+      stdout.write(`\n  Duration: ${lap()}\n\n`);
+    }
   };
 }
 
