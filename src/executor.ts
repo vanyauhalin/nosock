c05fcfa7c636126eb8969c3a10109952c04148ee
vanyauhalin <vanyauhalin@gmail.com>
2022-06-05 21:52:57 +0300

    feat: support inject colored values

--- src/executor.ts
+++ src/executor.ts
@@ -1,6 +1,6 @@
 import { env, stdout } from 'node:process';
 import type { Context } from './context';
-import { ACCENT, log } from './logger';
+import { log } from './logger';
 import { run } from './runner';
 import { stopwatch } from './utils';
 
@@ -9,15 +9,16 @@ function define(context: Context) {
     const lap = stopwatch();
     stdout.write('\n');
     if (file) log('Runnings scripts...').note(file);
+    let command;
     try {
-      const command = env['npm_lifecycle_event'];
+      command = env['npm_lifecycle_event'];
       if (!command) throw new Error('Missing a run command');
       const script = context.scripts[command];
-      if (!script) throw new Error(`The ${ACCENT(command)} is not described`);
+      if (!script) throw new Error('The %p is not described');
       await run(context, script);
     } catch (error) {
       const { message } = error as Error;
-      log.error(message);
+      log.error(message, command || 'run command');
       throw new Error(message);
     } finally {
       stdout.write(`\n  Duration: ${lap()}\n\n`);
--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,15 @@
 import { stderr, stdout } from 'node:process';
 import kleur from 'kleur';
 
-const ACCENT = kleur.blue;
-const SHADOW = kleur.gray;
-const DONE = `${kleur.green('done')}  `;
-const ERROR = `${kleur.red('error')} `;
-const WARN = `${kleur.yellow('warn')}  `;
+const ACCENT = kleur.magenta;
+const ACCENT_ATTENTION = kleur.yellow;
+const ACCENT_NEGATIVE = kleur.red;
+const ACCENT_POSITIVE = kleur.green;
+const NEUTRAL = kleur.gray;
+const PRIMARY = kleur.blue;
+const DONE = `${ACCENT_POSITIVE('done')}  `;
+const ERROR = `${ACCENT_NEGATIVE('error')} `;
+const WARN = `${ACCENT_ATTENTION('warn')}  `;
 const SHORT = '      ';
 const LONG = `               ${SHORT}`;
 const DATE = new Intl.DateTimeFormat('en-us', {
@@ -29,30 +33,53 @@ function align(head: string, body: string): string {
   return aligned;
 }
 
+function inject(body: string, values: string[]): string {
+  if (values.length === 0) return body;
+  const matched = body.match(/(%\w{2})|(%\w)/g);
+  if (!matched) return body;
+  let injected = body;
+  for (const flag of matched) {
+    const value = values.shift();
+    if (!value) break;
+    let modifier;
+    switch (flag) {
+      case '%a': modifier = ACCENT; break;
+      case '%aa': modifier = ACCENT_ATTENTION; break;
+      case '%an': modifier = ACCENT_NEGATIVE; break;
+      case '%ap': modifier = ACCENT_POSITIVE; break;
+      case '%n': modifier = NEUTRAL; break;
+      case '%p': modifier = PRIMARY; break;
+      default: break;
+    }
+    if (modifier) injected = injected.replace(flag, modifier(value));
+  }
+  return injected;
+}
+
 interface Logger {
-  (message: string): Logger;
-  done(message: string): Logger;
-  error(message: string): Logger;
-  note(message: string): Logger;
-  trace(error: Error): Logger;
-  warn(message: string): Logger;
+  (this: void, message: string, ...values: string[]): Logger;
+  done(this: void, message: string, ...values: string[]): Logger;
+  error(this: void, message: string, ...values: string[]): Logger;
+  note(this: void, message: string): Logger;
+  trace(this: void, error: Error): Logger;
+  warn(this: void, message: string, ...values: string[]): Logger;
 }
 
 const log: Logger = (() => {
-  function inner(message: string): Logger {
-    stdout.write(`${prefix()}${SHORT}${message}\n`);
+  function inner(message: string, ...values: string[]): Logger {
+    stdout.write(`${prefix()}${SHORT}${inject(message, values)}\n`);
     return inner;
   }
-  inner.done = (message: string) => {
-    stdout.write(`${prefix()}${DONE}${message}\n`);
+  inner.done = (message: string, ...values: string[]) => {
+    stdout.write(`${prefix()}${DONE}${inject(message, values)}\n`);
     return inner;
   };
-  inner.error = (message: string) => {
-    stderr.write(align(`${prefix()}${ERROR}`, message));
+  inner.error = (message: string, ...values: string[]) => {
+    stderr.write(align(`${prefix()}${ERROR}`, inject(message, values)));
     return inner;
   };
   inner.note = (message: string) => {
-    stdout.write(`${LONG}${SHADOW(message)}\n`);
+    stdout.write(`${LONG}${NEUTRAL(message)}\n`);
     return inner;
   };
   inner.trace = (error: Error) => {
@@ -67,11 +94,11 @@ const log: Logger = (() => {
     }
     return inner;
   };
-  inner.warn = (message: string) => {
-    stdout.write(`${prefix()}${WARN}${message}\n`);
+  inner.warn = (message: string, ...values: string[]) => {
+    stdout.write(`${prefix()}${WARN}${inject(message, values)}\n`);
     return inner;
   };
   return inner;
 })();
 
-export { ACCENT, log };
+export { log };
