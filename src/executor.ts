23d3acd492ff7ce7a5ada9f35ce73e78aeaf5bf5
vanyauhalin <vanyauhalin@gmail.com>
2022-06-08 16:29:24 +0300

    feat: update executor head dua new logger api

--- src/executor.ts
+++ src/executor.ts
@@ -1,4 +1,4 @@
-import { env, stdout } from 'node:process';
+import { env } from 'node:process';
 import type { Context } from './context';
 import { log } from './logger';
 import { run } from './runner';
@@ -11,19 +11,15 @@ function merge(values: string[]): string {
 function define(context: Context) {
   return async (file?: string): Promise<void> => {
     const lap = stopwatch();
-    stdout.write('\n');
-    if (file) log(`Runnings scripts ${file}`);
-    let command;
+    log.empty(file ? `\n  File:     ${file}` : '');
+    const command = env['npm_lifecycle_event'];
     try {
-      command = env['npm_lifecycle_event'];
       if (!command) throw new Error('Missing a run command');
       const script = context.scripts[command];
       if (!script) throw new Error('The %p is not described');
       await run(context, script);
     } catch (error) {
-      const { message } = error as Error;
-      log.error(message, command || 'run command');
-      throw new Error(message);
+      log.error((error as Error).message);
     } finally {
       let report = '';
       const { rejected, resolved } = context;
