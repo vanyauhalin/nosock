41df80d48bb0a13e82ff49aa72e5b4cf355ced6c
vanyauhalin <vanyauhalin@gmail.com>
2022-06-15 13:12:57 +0300

    test: remove delay

--- test/utils.ts
+++ test/utils.ts
@@ -2,19 +2,11 @@ import { suite } from 'uvu';
 import { is, match, type } from 'uvu/assert';
 import * as utils from '../lib/utils';
 
-const delay = suite('delay');
-
-delay('is a function', () => {
-  type(utils.delay, 'function');
-});
-
-delay('returns a promise', () => {
-  type(utils.delay(), 'object');
-});
-
-delay.run();
-
-// ---
+function delay(ms = 0): Promise<unknown> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 const stopwatch = suite('stopwatch');
 
@@ -33,14 +25,14 @@ stopwatch('lap matches the pattern', () => {
 stopwatch('the next lap is higher than the previous', async () => {
   const lap = utils.stopwatch();
   const first = Number.parseFloat(lap());
-  await utils.delay(1);
+  await delay(1);
   const second = Number.parseFloat(lap());
   is(second > first, true);
 });
 
 stopwatch('there is no cascade of laps', async () => {
   const firstLap = utils.stopwatch();
-  await utils.delay(1);
+  await delay(1);
   const secondLap = utils.stopwatch();
   const second = Number.parseFloat(secondLap());
   const first = Number.parseFloat(firstLap());
