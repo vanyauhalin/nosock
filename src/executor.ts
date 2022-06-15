a44839f2419c7a772a3ca0a296b8b9e11259c5c7
vanyauhalin <vanyauhalin@gmail.com>
2022-06-16 00:03:13 +0300

    feat: add an exit for a script with an error

--- src/executor.ts
+++ src/executor.ts
@@ -1,4 +1,4 @@
-import { env } from 'node:process';
+import { env, exit } from 'node:process';
 import type { Context } from './context';
 import { log } from './logger';
 import { run } from './runner';
@@ -20,7 +20,7 @@ function define(context: Context) {
   return async (options?: ExecutorOptions): Promise<void> => {
     const lap = stopwatch();
     context.options = { ...context.options, ...options };
-    const { history, store } = context;
+    const { history, state, store } = context;
     const { file, noCancel } = context.options;
     const command = env['npm_lifecycle_event'] || '';
 
@@ -62,9 +62,9 @@ function define(context: Context) {
         ['error', 'Rejected', 'an'],
       ];
       if (!noCancel) sections.push(['cancel', 'Canceled', 'aa']);
+
       const values = [];
       let message = '';
-
       for (const [type, section, flag] of sections) {
         const filtered = events.filter((event) => event.type === type);
         if (filtered.length > 0) {
@@ -73,8 +73,9 @@ function define(context: Context) {
           values.push(...commands);
         }
       }
-
       log.empty(`\n${message}  Duration: ${lap()}\n`, ...values);
+
+      if (state.hasError) exit(1);
     }
   };
 }
