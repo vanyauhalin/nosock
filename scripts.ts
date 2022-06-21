c1bdb64a287ae44fa911e61109ee2d7ed95b50f4
vanyauhalin <vanyauhalin@gmail.com>
2022-06-21 21:30:03 +0300

    build: add flat polyfill

--- scripts.ts
+++ scripts.ts
@@ -5,16 +5,50 @@ import type { BuildOptions } from 'esbuild';
 import { build as esbuild } from 'esbuild';
 import { script } from './src/index';
 
-async function oldie(
+async function polyfill(
   directory: string,
   file: string,
   replaces?: [string, string],
 ): Promise<void> {
   const resolved = join(directory, file.replace(extname(file), '.js'));
   const content = await readFile(resolved);
+  // NodeJS 10, 12 doesn't resolve `node` modules.
   let transformed = content.toString()
     .replace(/require\("node:(.+)"\)/g, 'require("$1")')
     .replace('require("fs/promises")', 'require("fs").promises');
+  /**
+   * esbuild doesn't transform `import.meta.url`.
+   * @see https://github.com/evanw/esbuild/issues/1492
+   */
+  if (transformed.includes('import_meta')) {
+    transformed = transformed.replace(
+      'import_meta = {}',
+      'import_meta = { url: require("url").pathToFileURL(__filename) }',
+    );
+  }
+  /**
+   * `Array.prototype.flat()` has been implemented since NodeJS 11.
+   * @see https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+   */
+  if (transformed.includes('.flat()')) {
+    transformed = transformed.replace(/(module.exports = __toCommonJS.*)/, `$1
+if (!Array.prototype.flat)
+  Object.defineProperty(Array.prototype, 'flat', {
+    configurable: true,
+    value: function flat() {
+      const depth = isNaN(arguments[0]) ? 1 : Number(arguments[0]);
+      return depth ? Array.prototype.reduce.call(this, (accumulator, current) => {
+        if (Array.isArray(current)) {
+          accumulator.push.apply(accumulator, flat.call(current, depth - 1));
+        } else {
+          accumulator.push(current);
+        }
+        return accumulator;
+      }, []) : Array.prototype.slice.call(this);
+    },
+    writable: true,
+  });`);
+  }
   if (replaces && replaces.length > 0) {
     transformed = transformed.replace(replaces[0], replaces[1]);
   }
@@ -38,10 +72,7 @@ async function build(output: string, toModules = false): Promise<void> {
       outdir: OUTPUT,
       target: 'node10',
     });
-    await oldie(OUTPUT, file, [
-      'import_meta = {}',
-      'import_meta = { url: require("url").pathToFileURL(__filename) }',
-    ]);
+    await polyfill(OUTPUT, file);
     if (!toModules) return;
     await esbuild({
       ...general,
@@ -90,7 +121,7 @@ script('build-ci', async () => {
         entryPoints: [join(TEST, file.name)],
         outdir: DISTRIBUTION_TEST,
       });
-      await oldie(DISTRIBUTION_TEST, file.name);
+      await polyfill(DISTRIBUTION_TEST, file.name);
     }));
   })();
   await script('build/scripts', async () => {
@@ -99,7 +130,7 @@ script('build-ci', async () => {
       entryPoints: ['scripts.ts'],
       outdir: DISTRIBUTION,
     });
-    await oldie(DISTRIBUTION, 'scripts.js', ['./src', './lib']);
+    await polyfill(DISTRIBUTION, 'scripts.js', ['./src', './lib']);
   })();
 });
 script('ci', test.bind(undefined, 'dist/test'));
